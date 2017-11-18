746ff08364ee854cec8123c407d260cba3b6c54b
dmnunez1993 <dmnunez1993@gmail.com>
2017-11-18 16:07:51 -0300

    added FKInlineAdmin

--- admin_utils/__init__.py
+++ admin_utils/__init__.py
@@ -1,5 +1,5 @@
 VERSION = '0.0.1'
 
-from admin_utils.admin import HiddenAdmin, HiddenAdminMixin
+from admin_utils.admin import HiddenAdmin, HiddenAdminMixin, FKInlineAdmin, FKInlineAdminMixin
 from admin_utils.models import AvailableModel, AvailableModelMixin, AvailableManager
 from admin_utils.forms import UniqueRelatedForm
